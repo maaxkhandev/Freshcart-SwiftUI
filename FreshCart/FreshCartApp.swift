0701639c2cae13e954d4b0d51bd274755e99ce31
maazkhan2 <maazkhanswat02@gmail.com>
2024-07-10 10:56:44 +0500

    Initial Commit

---
+++ FreshCart/FreshCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshCartApp.swift
+//  FreshCart
+//
+//  Created by Maaz Khan on 10/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FreshCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
